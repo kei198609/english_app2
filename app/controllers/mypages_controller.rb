55e22d72585f48f17a317d7458bef2da8d752ade
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-01-23 20:47:22 +0900

    投稿機能作成、ヘッダーにマイページ作成

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,2 +1,7 @@
 class MypagesController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # @user = User.page(params[:page]).per(5)
+    @posts = @user.posts.page(params[:page]).per(5)
+  end
 end
