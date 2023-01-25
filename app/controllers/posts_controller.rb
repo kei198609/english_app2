acb972415b8b9a721dd478aab9e07bc861bbd452
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-01-25 20:16:30 +0900

    投稿フォーム作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,28 @@
 class PostsController < ApplicationController
 before_action :authenticate_user!
 
- def new
- end
+  def new
+    @post = current_user.posts.build
+  end
 
- def create
- end
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "投稿しました！"
+      redirect_to root_url
+    else
+      render 'tops/index'
+    end
+  end
 
- def destroy
- end
+  def destroy
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
+
+
 end
