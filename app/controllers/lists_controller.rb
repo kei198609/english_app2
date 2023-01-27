5e79f619027ead7b43811e273d2c312066ae498e
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-01-27 09:03:34 +0900

    usersコントローラー作成,mypages,listsコントローラー削除

--- app/controllers/lists_controller.rb
+++
@@ -1,5 +0,0 @@
-class ListsController < ApplicationController
-  def index
-    @lists = User.page(params[:page]).per(5)
-  end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
-class MypagesController < ApplicationController
+class UsersController < ApplicationController
   before_action :authenticate_user!
+  def index
+    @users = User.page(params[:page]).per(5)
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).per(5)
