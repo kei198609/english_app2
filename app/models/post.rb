ae954eaa303c52af2231ff92b8b2dbf90f114cbb
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-03-03 08:13:49 +0900

    Action text導入

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   # validates :content, presence: true, length: { maximum: 500 }
   validates :content_english, presence: true, length: { maximum: 500 }
-  validates :content_japanese, presence: true, length: { maximum: 500 }
+  # validates :content_japanese, presence: true, length: { maximum: 500 }
   validates :subject_english, presence: true, length: { maximum: 100 }
-  validates :subject_japanese, presence: true, length: { maximum: 100 }
+  # validates :subject_japanese, presence: true, length: { maximum: 100 }
 
   # 検索機能
   scope :search, ->(keyword, scene = nil) {
@@ -14,4 +14,6 @@ class Post < ApplicationRecord
     query = query.where(scene: scene) if scene.present?
     query
   }
+  # Action Textを使用するため、has_rich_textメソッドを使用して、リレーションシップを設定
+  has_rich_text :content_english
 end
