ae5a52b17a581b3fc03295504b99caab72fd2fdd
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-03-04 07:31:01 +0900

    投稿、設定画面ブラッシュアップ

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,11 @@ class Post < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  # validates :content, presence: true, length: { maximum: 500 }
   validates :content_english, presence: true, length: { maximum: 500 }
-  # validates :content_japanese, presence: true, length: { maximum: 500 }
   validates :subject_english, presence: true, length: { maximum: 100 }
+  validates :scene, presence: true
+  # validates :content, presence: true, length: { maximum: 500 }
+  # validates :content_japanese, presence: true, length: { maximum: 500 }
   # validates :subject_japanese, presence: true, length: { maximum: 100 }
 
   # 検索機能
