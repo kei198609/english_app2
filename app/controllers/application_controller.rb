280002d4ba3634deb7ba5e016f2da3066c319c48
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-04-03 06:45:28 +0900

    マイページ/設定画面修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :occupation])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :occupation])
   end
 
   #ゲストユーザ特定するためresourceを用いる
