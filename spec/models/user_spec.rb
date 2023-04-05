90451c5d730a5ee572295867037ec36e9db03373
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-04-06 08:37:40 +0900

    rspec作成 userモデル2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # 名前、メール、職種、パスワードがあれば有効な状態であること
   it "is valid with a name, email, and password" do
-    # 名前、メール、職種、パスワードがあれば有効な状態であること
     user = User.new(
       name:       "Aaron",
       email:      "tester@example.com",
@@ -11,8 +11,8 @@ RSpec.describe User, type: :model do
     )
     expect(user).to be_valid
   end
+  # メールアドレスがなければ無効な状態であること
   it "is invalid without an email address" do
-    # メールアドレスがなければ無効な状態であること
     user = User.new(
       name:       "Aaron",
       email: nil,
@@ -22,5 +22,36 @@ RSpec.describe User, type: :model do
     expect(user).to_not be_valid
   end
 
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email address" do
+    User.create(
+      name:       "Joe",
+      email:      "tester@example.com",
+      occupation: "hoge",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    user = User.new(
+      name:       "Jane",
+      email:      "tester@example.com",
+      occupation: "hoge",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("は既に登録済みです。")
+  end
+
+  # 名前がなければ無効な状態であること
+  it "is invalid without an name" do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("が入力されていません。")
+  end
+  # 職種がなければ無効な状態であること
+  it "is invalid without an name" do
+    user = User.new(occupation: nil)
+    user.valid?
+    expect(user.errors[:occupation]).to include("が入力されていません。")
+  end
 end
 
