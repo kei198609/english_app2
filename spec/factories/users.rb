1f9a845e615125ffabd2338ba0cfaf29b7ed7d05
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-04-07 08:47:19 +0900

    rspec作成 userモデル3 factorybot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "Aaron" }
+    sequence(:email) { |n| "tester#{n}@example.com" }
+    occupation { "hoge" }
+    password { "dottle-nouveau-pavilion-tights-furze" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,55 +3,51 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # 名前、メール、職種、パスワードがあれば有効な状態であること
   it "is valid with a name, email, and password" do
-    user = User.new(
-      name:       "Aaron",
-      email:      "tester@example.com",
-      occupation: "hoge",
-      password:   "password",
-    )
-    expect(user).to be_valid
+    expect(FactoryBot.build(:user)).to be_valid
   end
   # メールアドレスがなければ無効な状態であること
   it "is invalid without an email address" do
-    user = User.new(
-      name:       "Aaron",
-      email: nil,
-      occupation: "hoge",
-      password:   "password"
-    )
+    user = FactoryBot.build(:user, email: nil)
     expect(user).to_not be_valid
   end
 
   # 重複したメールアドレスなら無効な状態であること
   it "is invalid with a duplicate email address" do
-    User.create(
-      name:       "Joe",
-      email:      "tester@example.com",
-      occupation: "hoge",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    user = User.new(
-      name:       "Jane",
-      email:      "tester@example.com",
-      occupation: "hoge",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
     user.valid?
     expect(user.errors[:email]).to include("は既に登録済みです。")
   end
 
   # 名前がなければ無効な状態であること
   it "is invalid without an name" do
-    user = User.new(name: nil)
+    user = FactoryBot.build(:user, name: nil)
     user.valid?
     expect(user.errors[:name]).to include("が入力されていません。")
   end
   # 職種がなければ無効な状態であること
   it "is invalid without an name" do
-    user = User.new(occupation: nil)
+    user = FactoryBot.build(:user, occupation: nil)
     user.valid?
     expect(user.errors[:occupation]).to include("が入力されていません。")
   end
-end
+  # メールアドレスがなければ無効な状態であること
+  it "is invalid without an email" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("が入力されていません。")
+  end
+  #名前が10文字以上なら無効な状態であること
+  it "is name should not be too long" do
+    user = FactoryBot.build(:user, name: "a" * 11)
+    user.valid?
+    expect(user.errors[:name]).to include("は10文字以内で入力してください。")
+  end
+  #パスワードが6文字未満なら無効な状態であること
+  it "is password should not be too short" do
+    user = FactoryBot.build(:user, password: "a" * 5)
+    user.valid?
+    expect(user.errors[:password]).to include("は6文字以上で入力してください。")
+  end
 
+end
