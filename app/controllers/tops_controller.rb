01038dbd6601418babb82e4beafdd6c19ed11f02
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-04-17 06:37:27 +0900

    レコメンド機能追加

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -9,6 +9,12 @@ class TopsController < ApplicationController
       else
         @feed_items = current_user.feed.page(params[:page]).per(8)
       end
+      occupation_posts = User.where(occupation: current_user.occupation).pluck(:id)
+      @recommend_post = Post.where(user_id: occupation_posts).
+                        where.not(user_id: current_user.id).
+                        joins(:likes).
+                        group(:id).
+                        order('MAX(likes.created_at) DESC').first
     end
   end
 end
