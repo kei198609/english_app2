d1b0302b24f2445ef02427773ea1b5bd2e86b110
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-05-21 08:15:09 +0900

    Add Home Toolbar

---
+++ front/components/Home/HomeAppBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app-bar
+    app
+    dark
+  >
+    <app-logo />
+
+    <v-toolbar-title>
+      Biz mail English
+    </v-toolbar-title>
+
+    <v-spacer />
+
+    <v-toolbar-items class="ml-2">
+      <v-btn
+        v-for="(menu, i) in menus"
+        :key="`menu-btn-${i}`"
+        text
+      >
+        {{ $t(`menus.${menu.title}`) }}
+      </v-btn>
+    </v-toolbar-items>
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  props: {
+    menus: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,90 +1,91 @@
 <template>
-  <v-container fluid>
-    <v-card
-      flat
-      tile
-      color="transparent"
+  <v-app>
+    <home-app-bar
+      :menus="menus"
+    />
+
+    <v-img
+      dark
+      src="https://picsum.photos/id/20/1920/1080?blur=5"
+      gradient="to top right, rgba(19,84,122,.6), rgba(128,208,199,.9)"
+      :height="imgHeight"
     >
-      <v-card-title>
-        Usersテーブルの取得
-      </v-card-title>
-
-      <v-card-title>
-        Vuetifyの導入（オリジナルカラーの確認）
-      </v-card-title>
-      <v-card-text>
-        <v-btn
-          v-for="(color, i) in colors"
-          :key="`color-${i}`"
-          :color="color"
-          class="mr-2"
+      <v-row
+        align="center"
+        justify="center"
+        :style="{ height: `${imgHeight}px` }"
+      >
+        <v-col
+          cols="12"
+          class="text-center"
         >
-          {{ color }}
-        </v-btn>
-      </v-card-text>
+          <h1 class="display-1 mb-4">
+            未来を作ろう。ワクワクしよう。
+          </h1>
+          <h4
+            class="subheading"
+            :style="{ letterSpacing: '5px' }"
+          >
+            中小企業に特化した事業計画策定ツール
+          </h4>
+        </v-col>
+      </v-row>
+    </v-img>
 
-      <v-card-title>
-      VuetifyカスタムCSSの検証
-      </v-card-title>
-      <v-card-text>
-        ipad（768px）とmobile（426px）で表示・非表示
-      </v-card-text>
-      <v-card-text>
-        <v-card
-          v-for="(cls, i) in customClass"
-          :key="`cls-${i}`"
-          :color="cls.color"
-          :class="cls.name"
+    <v-sheet>
+      <v-container
+        fluid
+        :style="{ maxWidth: '1280px' }"
+      >
+        <v-row
+          v-for="(menu, i) in menus"
+          :key="`menu-${i}`"
         >
-          <v-card-text>
-            {{ cls.des }}
-          </v-card-text>
-        </v-card>
-      </v-card-text>
 
-      <v-card-title>
-        nuxt-i18nの検証
-      </v-card-title>
-      <v-card-text>
-        <v-simple-table dense>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th>en</th>
-                <th>ja</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(path, i) in ['signup', 'login']"
-                :key="`path-${i}`"
-              >
-                <td>{{ path }}</td>
-                <td>{{ $t(`title.${path}`) }}</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-card-text>
+        <v-col
+          cols="12"
+        >
+          <v-card flat>
+            <v-card-title class="justify-center display-1">
+              {{ $t(`menus.${menu.title}`) }}
+            </v-card-title>
+            <v-card-text class="text-center">
+              {{ menu.subtitle }}
+            </v-card-text>
+          </v-card>
+        </v-col>
 
-    </v-card>
-  </v-container>
+          <v-col
+            cols="12"
+          >
+            <div
+              :is="`home-${menu.title}`"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
+    <app-footer />
+  </v-app>
 </template>
 
 <script>
-export default {
+import HomeAbout from '~/components/Home/HomeAbout'
+import HomeProducts from '~/components/Home/HomeProducts'
 
+export default {
+  components: {
+    HomeAbout,
+    HomeProducts
+  },
   data () {
     return {
-      colors: ['primary', 'info', 'success', 'warning', 'error', 'background'],
-      customClass: [
-        { name: 'hidden-ipad-and-down', color: 'error', des: 'ipad未満で隠す' },
-        { name: 'hidden-ipad-and-up', color: 'info', des: 'ipad以上で隠す' },
-        { name: 'hidden-mobile-and-down', color: 'success', des: 'mobile未満で隠す' },
-        { name: 'hidden-mobile-and-up', color: 'warning', des: 'mobile以上で隠す' }
+      imgHeight: 500,
+      menus: [
+        { title: 'about', subtitle: 'このサイトはブログ"独学プログラマ"で公開されているチュートリアルのデモアプリケーションです' },
+        { title: 'products', subtitle: '他にはない優れた機能の数々' }
       ]
     }
   }
-
 }
 </script>
