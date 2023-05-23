c1872dce9ef586025ac5a8bde896fca4703c62bf
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-05-24 06:39:09 +0900

    create_signup_login_layout_files

---
+++ front/components/BeforeLogin/BeforeLoginAppBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar
+    app
+    :height="homeAppBarHeight"
+    color="white"
+  >
+    <nuxt-link
+      to="/"
+      class="text-decoration-none"
+    >
+      <app-logo />
+    </nuxt-link>
+
+    <v-toolbar-title class="hidden-mobile-and-down">
+      Biz mail English
+    </v-toolbar-title>
+
+    <v-spacer />
+
+    <app-signup-button />
+    <app-login-button />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data ({ $store }) {
+    return {
+      homeAppBarHeight: $store.state.styles.homeAppBarHeight
+    }
+  }
+}
+</script>
