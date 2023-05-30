5f1dcd575d6b9d8ed3e5c997933adf6824955ba6
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-05-30 20:17:04 +0900

    add_navigation_drawer

---
+++ front/components/Top/TopNavigationDrawer.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-navigation-drawer
+    v-model="setDrawer"
+    app
+    clipped
+    :mobile-breakpoint="mobileBreakpoint"
+  >
+    <v-list>
+      <!-- close-button -->
+      <template
+      v-if="isBreakpoint"
+      >
+        <v-list-item
+          @click="$emit('update:drawer', false)"
+        >
+          <v-list-item-content>
+            <v-list-item-action-text>
+              <v-icon>
+                mdi-chevron-double-left
+              </v-icon>
+              閉じる
+            </v-list-item-action-text>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider />
+      </template>
+      <!-- nav menus -->
+      <v-list-item
+        v-for="(nav, i) in navMenus"
+        :key="`nav-${i}`"
+        :to="$my.projectLinkTo($route.params.id, nav.name)"
+      >
+        <v-list-item-icon>
+          <v-icon>{{ nav.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ $my.pageTitle(nav.name) }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: {
+    drawer: {
+      type: Boolean,
+      default: null
+    }
+  },
+  data () {
+    return {
+      mobileBreakpoint: 960,
+      navMenus: [
+        { name: 'top-id-users', icon: 'mdi-view-dashboard' },
+        { name: 'top-id-bookmark', icon: 'mdi-view-compact' },
+        { name: 'top-id-mypage', icon: 'mdi-image' },
+        { name: 'top-id-post', icon: 'mdi-view-comfy' },
+        { name: 'top-id-notifications', icon: 'mdi-cog' },
+        { name: 'top-id-settings', icon: 'mdi-help-circle' }
+      ]
+    }
+  },
+  computed: {
+    setDrawer: {
+      get () { return this.drawer },
+      set (newVal) { return this.$emit('update:drawer', newVal) }
+    },
+    isBreakpoint () {
+      const windowWidth = this.$vuetify.breakpoint.width
+      return this.mobileBreakpoint > windowWidth
+    }
+  }
+}
+</script>
--- front/pages/tops.vue
+++ front/pages/tops.vue
@@ -2,116 +2,131 @@
   <div
     id="tops"
   >
-    <v-parallax>
-      <v-img
-        :src="homeImg"
-        alt="homeImg"
-        :aspect-ratio="16/9"
-        gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
-      >
-        <v-container
-          fill-height
+    <v-app>
+      <logged-in-app-bar clipped-left>
+        <template v-slot:navigation-toggle-button>
+          <v-app-bar-nav-icon
+            @click="drawer = !drawer"
+          />
+        </template>
+      </logged-in-app-bar>
+      <!-- ナビゲーションドロワー -->
+      <top-navigation-drawer
+        :drawer.sync="drawer"
+      />
+      <!-- メインコンテンツ -->
+      <v-parallax>
+        <v-img
+          :src="homeImg"
+          alt="homeImg"
+          :aspect-ratio="16/9"
+          gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
         >
-          <v-row
-            justify="center"
-            align="center"
+          <v-container
+            fill-height
           >
-            <v-col
-              cols="12"
-              :sm="container.sm"
-              :md="container.md"
+            <v-row
+              justify="center"
+              align="center"
             >
-              <v-card-title
-                class="white--text"
+              <v-col
+                cols="12"
+                :sm="container.sm"
+                :md="container.md"
               >
-                最近のプロジェクト
-              </v-card-title>
-
-              <v-divider
-                dark
-                class="mb-4"
-              />
-
-              <v-row
-                align="center"
-              >
-                <v-col
-                  cols="12"
-                  :sm="card.sm"
-                  :md="card.md"
+                <v-card-title
+                  class="white--text"
                 >
-                  <v-btn
-                    block
-                    :height="card.height"
-                    :elevation="card.elevation"
+                  最近のプロジェクト
+                </v-card-title>
+
+                <v-divider
+                  dark
+                  class="mb-4"
+                />
+
+                <v-row
+                  align="center"
+                >
+                  <v-col
+                    cols="12"
+                    :sm="card.sm"
+                    :md="card.md"
                   >
-                    <div>
-                      <v-icon
-                        size="24"
-                        color="myblue"
-                        class="my-2"
-                      >
-                        mdi-plus
-                      </v-icon>
-                      <div
-                        class="caption myblue--text"
-                      >
-                        プロジェクトを追加
+                    <v-btn
+                      block
+                      :height="card.height"
+                      :elevation="card.elevation"
+                    >
+                      <div>
+                        <v-icon
+                          size="24"
+                          color="myblue"
+                          class="my-2"
+                        >
+                          mdi-plus
+                        </v-icon>
+                        <div
+                          class="caption myblue--text"
+                        >
+                          プロジェクトを追加
+                        </div>
                       </div>
-                    </div>
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-img>
-    </v-parallax>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-img>
+      </v-parallax>
 
-    <v-container>
-      <v-row justify="center">
-        <v-col
-          cols="12"
-          :sm="container.sm"
-          :md="container.md"
-        >
-          <v-card-title>
-            全てのプロジェクト
-          </v-card-title>
-
-          <v-divider class="mb-4" />
-
-          <v-data-table
-            :headers="tableHeaders"
-            :items="recentProjects"
-            item-key="id"
-            hide-default-footer
+      <v-container>
+        <v-row justify="center">
+          <v-col
+            cols="12"
+            :sm="container.sm"
+            :md="container.md"
           >
-            <template v-slot:[`item.name`]="{ item }">
-              <nuxt-link
-                :to="$my.projectLinkTo(item.id)"
-                class="text-decoration-none"
-              >
-                {{ item.name }}
-              </nuxt-link>
-            </template>
-            <template v-slot:[`item.updatedAt`]="{ item }">
-              {{ $my.dataFormat(item.updatedAt) }}
-            </template>
-          </v-data-table>
-        </v-col>
-      </v-row>
-    </v-container>
+            <v-card-title>
+              全てのプロジェクト
+            </v-card-title>
 
+            <v-divider class="mb-4" />
+
+            <v-data-table
+              :headers="tableHeaders"
+              :items="recentProjects"
+              item-key="id"
+              hide-default-footer
+            >
+              <template v-slot:[`item.name`]="{ item }">
+                <nuxt-link
+                  :to="$my.projectLinkTo(item.id)"
+                  class="text-decoration-none"
+                >
+                  {{ item.name }}
+                </nuxt-link>
+              </template>
+              <template v-slot:[`item.updatedAt`]="{ item }">
+                {{ $my.dataFormat(item.updatedAt) }}
+              </template>
+            </v-data-table>
+          </v-col>
+        </v-row>
+      </v-container>
+
+    </v-app>
   </div>
 </template>
 
 <script>
 import homeImg from '~/assets/images/logged-in/home.png'
 export default {
-  layout: 'logged-in',
+  // layout: 'logged-in'6行目と重複しているのでコメントアウト,
   data () {
     return {
+      drawer: null,
       homeImg,
       container: {
         sm: 10,
