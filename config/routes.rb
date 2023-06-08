8ea8f4d7e25774fd6fe565811ccfa54644a31141
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-06-09 07:54:18 +0900

    Change_routes_for_devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 
   devise_for :users, skip: [:sessions, :passwords, :registrations]
 
-  namespace :api, defaults: { format: :json } do
-    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+    end
   end
 
   devise_scope :user do #deviseのsessionsコントローラーに新しいactionを追加するためにはdevise_scopeと書く
