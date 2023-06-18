00bc408585d61698ca0aee27d266fd08262c2c6a
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-06-19 06:45:02 +0900

    Create_logout.vue

--- front/pages/logout.vue
+++ front/pages/logout.vue
@@ -1,12 +1,21 @@
+<template>
+  <div>ログアウトしています...</div>
+</template>
+
 <script>
-// export default {
-//   methods: {
-//     logout () {
-//       this.$auth.logout()
-//         .then(() => {
-//           this.$router.push('/')
-//         })
-//     }
-//   }
-// }
+export default {
+  mounted () {
+    this.logout()
+  },
+  methods: {
+    async logout () {
+      try {
+        await this.$auth.logout()
+        this.$router.push('/')
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+}
 </script>
