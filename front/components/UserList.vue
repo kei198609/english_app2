1277a7cce1621a6426e8b0c960b649570a79db0d
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-06-20 11:56:36 +0900

    ユーザ一覧画面作成

---
+++ front/components/UserList.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-list-item>
+    <v-list-item-avatar>
+      <v-img :src="user.avatar || noimage"
+              @click="$router.push(`/users/${user.id}`)"
+              class="clickable-image">
+      </v-img>
+    </v-list-item-avatar>
+    <v-list-item-content>
+      <v-list-item-title>
+        <nuxt-link :to="`/users/${user.id}`">{{ user.name }}</nuxt-link>
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      noimage: require('~/assets/images/noimage.jpg')
+    }
+  },
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .clickable-image {
+    cursor: pointer;
+  }
+</style>
--- front/pages/top/_id/users.vue
+++ front/pages/top/_id/users.vue
@@ -1,10 +1,59 @@
 <template>
-  <div>
-    {{ $route.fullPath }}
-  </div>
+  <v-app>
+    <logged-in-app-bar clipped-left>
+      <template v-slot:navigation-toggle-button>
+        <v-app-bar-nav-icon
+          @click="drawer = !drawer"
+        />
+      </template>
+    </logged-in-app-bar>
+    <!-- ナビゲーションドロワー -->
+    <top-navigation-drawer
+      :drawer.sync="drawer"
+    />
+    <v-main>
+      <v-container>
+        <v-list>
+          <User
+            v-for="user in users"
+            :key="user.id"
+            :user="user"
+          />
+        </v-list>
+        <v-pagination
+          v-model="current_page"
+          :length="total_pages"
+          @input="fetchUsers"
+        />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import User from '~/components/UserList.vue'
+
 export default {
+  components: {
+    User
+  },
+  data () {
+    return {
+      drawer: null,
+      users: [],
+      total_pages: 0,
+      current_page: 1
+    }
+  },
+  async created () {
+    await this.fetchUsers()
+  },
+  methods: {
+    async fetchUsers () {
+      const response = await this.$axios.get(`http://localhost:3000/api/v1/users?page=${this.current_page}`)
+      this.users = response.data.users
+      this.total_pages = response.data.total_pages
+    }
+  }
 }
 </script>
