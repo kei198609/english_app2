cdb631445d072ebe1b8659b0eb2a3d0bc4076dc9
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-06-22 21:40:01 +0900

    api移行につき一部コメントアウト

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate_user!
-  # before_action :correct_user, only:[:show]
 
   def index
     @users = User.page(params[:page]).per(5)
@@ -13,11 +12,18 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.page(params[:page]).per(5)
-    @data = @user.posts.group_by(&:scene).transform_values(&:count)
+    # @user = User.find_by(uid: params[:uid])
+    # @posts = @user.posts.page(params[:page]).per(5)
+    # @data = @user.posts.group_by(&:scene).transform_values(&:count)
 
-    bookmarks = Bookmark.where(user_id: current_user.id).pluck(:post_id)  # ログイン中のユーザーのブックマークのpost_idカラムを取得
-    @bookmark_list = Post.find(bookmarks)     # postsテーブルから、お気に入り登録済みのレコードを取得
+    # bookmarks = Bookmark.where(user_id: current_user.id).pluck(:post_id)  # ログイン中のユーザーのブックマークのpost_idカラムを取得
+    # @bookmark_list = Post.find(bookmarks)     # postsテーブルから、お気に入り登録済みのレコードを取得
+    render json: {
+      user: @user
+      # posts: @posts,
+      # data: @data,
+      # bookmark_list: @bookmark_list
+    }
   end
 
   def following
@@ -34,10 +40,4 @@ class Api::V1::UsersController < ApplicationController
     render 'show_follow'
   end
 
-
-  # private
-  #   def correct_user
-  #     @user = User.find(params[:id])
-  #     redirect_to(root_path) unless @user == current_user
-  #   end
 end
