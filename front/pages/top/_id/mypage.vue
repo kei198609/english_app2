1dfab8a8aaf9feb17badc56b34bc4a491da6f63e
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-06-30 07:40:35 +0900

    マイページ修正

--- front/pages/top/_id/mypage.vue
+++ front/pages/top/_id/mypage.vue
@@ -1,12 +1,27 @@
 <template>
-  <v-container>
-    <v-row>
-      <!-- User section -->
-      <v-col cols="4" class="mt-3">
-        <UserSection v-if="user" :user="user" />
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-app>
+    <logged-in-app-bar clipped-left>
+      <template v-slot:navigation-toggle-button>
+        <v-app-bar-nav-icon
+          @click="drawer = !drawer"
+        />
+      </template>
+    </logged-in-app-bar>
+    <!-- ナビゲーションドロワー -->
+    <top-navigation-drawer
+      :drawer.sync="drawer"
+    />
+    <v-main>
+      <v-container>
+        <v-row>
+          <!-- User section -->
+          <v-col cols="4" class="mt-3">
+            <UserSection v-if="user" :user="user" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -19,15 +34,21 @@ export default {
   },
   data () {
     return {
+      drawer: null,
       user: null
     }
   },
   async fetch () {
     console.log('fetch started')
     console.log('loggedIn:', this.$auth.loggedIn)
+    console.log(this.$route.params.id)
     try {
       if (this.$auth.loggedIn) {
-        const response = await this.$axios.get(`http://localhost:3000/api/v1/users/${this.$auth.user.id}`)
+        // this.$route.params.idがundefinedであればthis.$auth.user.idを使用し、それ以外の場合はthis.$route.params.idを使用するという条件分岐
+        const userId = this.$route.params.id ? this.$route.params.id : this.$auth.user.id
+        const response = await this.$axios.get(`http://localhost:3000/api/v1/users/${userId}`)
+        // const response = await this.$axios.get(`http://localhost:3000/api/v1/users/${this.$route.params.id}`)
+        // const response = await this.$axios.get(`http://localhost:3000/api/v1/users/${this.$auth.user.id}`)
         this.user = response.data.user
       }
     } catch (error) {
