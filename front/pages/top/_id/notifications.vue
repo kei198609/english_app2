f453386f9c8bdd2754cf903103c2976190b834d4
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-07-01 07:18:57 +0900

    ナビゲーションドロワー設置

--- front/pages/top/_id/notifications.vue
+++ front/pages/top/_id/notifications.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>
-    {{ $route.fullPath }}
-  </div>
+  <v-app>
+    <logged-in-app-bar clipped-left>
+      <template v-slot:navigation-toggle-button>
+        <v-app-bar-nav-icon
+          @click="drawer = !drawer"
+        />
+      </template>
+    </logged-in-app-bar>
+    <!-- ナビゲーションドロワー -->
+    <top-navigation-drawer
+      :drawer.sync="drawer"
+    />
+    <v-main>
+      <v-container>
+        <div>
+          {{ $route.fullPath }}
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      drawer: null
+    }
+  }
 }
 </script>
