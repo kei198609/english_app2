1f35a2960e3e1af8df3f504cf7f6929cdf5e3b64
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-07-03 06:41:13 +0900

    リンクリスト作成

---
+++ front/components/LinkList.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-col cols="4">
+    <v-list dense>
+      <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item-content>
+          <v-list-item-title>
+            <router-link :to="item.link">{{ item.title }}</router-link>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: ['items']
+}
+</script>
