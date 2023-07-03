ec00893aaaf7b4a06c27cf1f64e92de91121d4a6
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-07-04 08:26:51 +0900

    サインアップ画面に職種選択フォーム作成

--- front/components/User/UserFormOccupation.vue
+++ front/components/User/UserFormOccupation.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-text-field
+  <v-select
     v-model="setOccupation"
     :rules="rules"
-    :counter="max"
-    label="職種を入力"
+    :items="items"
+    label="職種を選択"
     placeholder="あなたの職種"
     outlined
   />
@@ -18,12 +18,12 @@ export default {
     }
   },
   data () {
-    const max = 30
     return {
-      max,
+      items: ['営業', '企画・マーケティング', '一般事務', '販売・サービス関連', '専門職(コンサルタント・金融・不動産等)',
+        '経理・財務', '人事', '総務', '技術系(ITエンジニア)', '技術系(メディカル、科学、食品、医療)',
+        '技術系(電気、電子、機械、自動車)', '技術系(建築、土木関連)', 'クリエイティブ関連', '専門職(インターネット)関連', 'その他'],
       rules: [
-        v => !!v || '',
-        v => (!!v && max >= v.length) || `${max}文字以内で入力してください`
+        v => !!v || '職種を選択してください'
       ]
     }
   },
