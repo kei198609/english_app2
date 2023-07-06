1c2fed854425fec4b39b18142ed13b5fd1df041e
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-07-07 08:43:51 +0900

    メールアドレス変更機能作成

---
+++ front/pages/top/_id/email.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-app>
+    <logged-in-app-bar clipped-left>
+      <template v-slot:navigation-toggle-button>
+        <v-app-bar-nav-icon
+          @click="drawer = !drawer"
+        />
+      </template>
+    </logged-in-app-bar>
+    <!-- ナビゲーションドロワー -->
+    <top-navigation-drawer
+      :drawer.sync="drawer"
+    />
+    <v-main>
+      <v-container>
+        <v-row>
+          <link-list :items="items" />
+          <v-col cols="8">
+            <div>メールアドレス</div>
+            <user-from-card-change>
+              <template #user-form-card-change-content>
+                <v-form ref="form" v-model="valid">
+                  <user-form-email
+                    :email.sync="params.user.email"
+                  />
+                  <v-btn
+                    :disabled="!valid"
+                    color="primary"
+                    @click="changeEmail"
+                  >
+                    保存
+                  </v-btn>
+                </v-form>
+              </template>
+            </user-from-card-change>
+          </v-col>
+          <v-snackbar v-model="snackbar" bottom right color="success">
+            {{ snackbarMessage }}
+            <v-btn color="white" text @click="snackbar = false">
+              閉じる
+            </v-btn>
+          </v-snackbar>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import UserFormEmail from '~/components/User/UserFormEmail.vue'
+import UserFromCardChange from '~/components/User/UserFromCardChange.vue'
+import LinkList from '~/components/LinkList.vue'
+
+export default {
+  components: { UserFormEmail, UserFromCardChange, LinkList },
+  data () {
+    return {
+      valid: true,
+      snackbar: false,
+      snackbarMessage: '',
+      params: {
+        user: {
+          email: ''
+        }
+      },
+      drawer: null,
+      items: [
+        { title: 'アカウント', link: `/top/${this.$auth.user.id}/settings` },
+        { title: '公開用プロフィール', link: '' },
+        { title: 'パスワード', link: `/top/${this.$auth.user.id}/password` },
+        { title: 'メールアドレス', link: `/top/${this.$auth.user.id}/email` }
+      ]
+    }
+  },
+  methods: {
+    async changeEmail () {
+      if (this.$refs.form.validate()) {
+        try {
+          const response = await this.$axios.put('/api/v1/auth', {
+            user: {
+              email: this.params.user.email
+            }
+          }, {
+            headers: {
+              'access-token': localStorage.getItem('access-token'),
+              client: localStorage.getItem('client'),
+              uid: localStorage.getItem('uid')
+            }
+          })
+          console.log(response)
+          // Email change was successful
+          this.snackbar = true
+          this.snackbarMessage = 'メールアドレスが変更されました'
+          this.params.user.email = ''
+        } catch (err) {
+          console.error(err)
+          if (err.response && err.response.status === 500) {
+            this.snackbar = true
+            this.snackbarMessage = '無効なメールアドレスです'
+          }
+        }
+      }
+    }
+  }
+}
+</script>
