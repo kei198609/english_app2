52b85165d95cf987918fa982c1544f266b505b6a
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-07-20 12:47:01 +0900

    自分の投稿リストをmypageに作成

---
+++ front/components/Post/PostListItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <v-row class="bg-white">
+      <v-col class="d-flex border-top pt-3">
+        <div>
+          <v-img
+            :src="post.user.avatar.url || 'noimage'"
+            class="rounded-circle"
+            width="32"
+            height="32"
+          />
+        </div>
+        <div class="post_user">
+          <p class="ms-1 mb-0">
+            <router-link to="`/user/${post.user.id}`" class="link-dark">
+              {{ post.user.name }}
+            </router-link>
+          </p>
+          <p class="post_time link-secondary ms-1 mb-0">
+            {{ formatDate(post.created_at) }}
+          </p>
+        </div>
+        <div class="ms-auto">
+          <!-- 削除機能はサーバーサイドとの通信が必要です -->
+        </div>
+      </v-col>
+      <v-col class="post_subject ps-5">
+        <router-link to="`/post/${post.id}`" class="link-dark">
+          {{ post.subject_english }}
+        </router-link>
+      </v-col>
+      <v-col class="post_scene ps-5 pb-3">
+        <p class="mb-0">{{ post.scene }}</p>
+        <p class="mb-0">
+          <v-icon class="fa-regular fa-heart text-center me-1" style="color: #AAAAAA;">mdi-heart</v-icon>
+          {{ post.likes_count }}
+          <v-icon class="fa-regular fa-comment text-center ms-3 me-1" style="color: #AAAAAA;">mdi-comment</v-icon>
+          {{ post.comments_count }}
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    formatDate (dateString) {
+      return moment(dateString).format('YYYY-MM-DD HH:mm')
+    }
+  }
+}
+</script>
--- front/pages/top/_id/mypage.vue
+++ front/pages/top/_id/mypage.vue
@@ -18,9 +18,15 @@
           <v-col cols="12" md="4" lg="4" class="mt-3">
             <UserSection v-if="user" :user="user" />
           </v-col>
-          <!-- Chart section -->
+          <!-- Chart and Posts section -->
           <v-col cols="8" class="mt-3">
             <CustomPieChart v-if="data" :chart-data="data"></CustomPieChart>
+            <v-subheader>Posts</v-subheader>
+            <v-list v-if="posts" class="mt-3">
+              <v-list-item  v-for="post in posts" :key="post.id">
+                <PostListItem :post="post" />
+              </v-list-item>
+            </v-list>
           </v-col>
         </v-row>
       </v-container>
@@ -31,18 +37,21 @@
 <script>
 import UserSection from '~/components/UserSection.vue'
 import CustomPieChart from '~/components/CustomPieChart.vue'
+import PostListItem from '~/components/Post/PostListItem.vue'
 
 export default {
   components: {
     UserSection,
-    CustomPieChart
+    CustomPieChart,
+    PostListItem
   },
   // data: nullにテストデータ表示させ円グラフ描画表示検証済み
   data () {
     return {
       drawer: null,
       user: null,
-      data: null
+      data: null,
+      posts: null
     }
   },
   async fetch () {
@@ -54,7 +63,13 @@ export default {
         const userId = this.$route.params.id ? this.$route.params.id : this.$auth.user.id
         const response = await this.$axios.get(`/api/v1/users/${userId}`)
         this.user = response.data.user
+        this.user.followers_count = response.data.followers_count
+        this.user.following_count = response.data.following_count
         this.data = response.data.data
+
+        const postResponse = await this.$axios.get(`/api/v1/posts?user_id=${userId}`)
+        this.posts = postResponse.data.posts
+        console.log(this.posts) // ここでレスポンスの内容を確認します
       }
     } catch (error) {
       console.error('Error fetching data', error)
@@ -64,3 +79,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.v-list-item {
+  border-bottom: 1px solid #ddd
+}
+
+.v-list-item:hover {
+  background-color: lightgray
+}
+</style>
