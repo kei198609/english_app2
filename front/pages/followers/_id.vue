1523815bf270e53e7554ce843b6b99458c6d1790
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-07-22 07:17:46 +0900

    フォロー,フォロワー画面作成

---
+++ front/pages/followers/_id.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <logged-in-app-bar clipped-left>
+      <template v-slot:navigation-toggle-button>
+        <v-app-bar-nav-icon
+          @click="drawer = !drawer"
+        />
+      </template>
+    </logged-in-app-bar>
+    <!-- ナビゲーションドロワー -->
+    <top-navigation-drawer
+      :drawer.sync="drawer"
+    />
+    <v-main>
+      <v-container>
+        <v-list>
+          <UserList
+            v-for="user in users"
+            :key="user.id"
+            :user="user"
+          />
+        </v-list>
+        <v-pagination
+          v-model="current_page"
+          :length="total_pages"
+          @input="fetchUsers"
+        />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import UserList from '~/components/UserList.vue'
+
+export default {
+  components: {
+    UserList
+  },
+  data () {
+    return {
+      drawer: null,
+      users: [],
+      total_pages: 0,
+      current_page: 1
+    }
+  },
+  async created () {
+    await this.fetchUsers()
+  },
+  methods: {
+    async fetchUsers () {
+      const response = await this.$axios.get(`/api/v1/users/${this.$route.params.id}/followers?page=${this.current_page}`)
+      this.users = response.data.users
+      this.total_pages = response.data.total_pages
+      this.current_page = response.data.current_page
+    }
+  }
+}
+</script>
