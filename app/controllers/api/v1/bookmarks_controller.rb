5fd26d3864b707372c22562f680d50b3584b1227
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-07-31 06:45:01 +0900

    api用bookmarks_controller作成

---
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::BookmarksController < ApplicationController
+
+  before_action :authenticate_user!   # ログイン中のユーザーのみに許可（未ログインなら、ログイン画面へ移動）
+
+  def index
+    @bookmarks = Bookmark.where(user_id: current_user.id).page(params[:page]).per(10)
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    bookmark = @post.bookmarks.new(user_id: current_user.id)
+    bookmark.save
+    render json: {
+      bookmarked: true,
+      bookmark_id: bookmark.id
+    }
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    bookmark = @post.bookmarks.find_by(user_id: current_user.id)
+    bookmark&.destroy
+    render json: {
+      bookmarked: false
+    }
+  end
+end
