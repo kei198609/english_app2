4d9c480a428b2d722be918f57178329a2daf8f61
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-08-12 07:15:30 +0900

    経験値機能のためメソッド追加

--- app/models/scene_experience.rb
+++ app/models/scene_experience.rb
@@ -1,4 +1,18 @@
 class SceneExperience < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+
+  validates :experience_scene, presence: true
+  validates :xp, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  after_save :check_for_badge
+
+  MAX_XP = 3 # 3回投稿でゲージマックス
+
+  private
+
+  def check_for_badge
+    if user.scene_experiences.all? { |exp| exp.xp >= MAX_XP }
+      user.update(has_clear_badge: true)
+    end
+  end
 end
