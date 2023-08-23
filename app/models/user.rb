c65e87895a489a23101c02eca16bc9760e4379ed
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-08-24 06:44:42 +0900

    quiz_attemptsモデルへの関連付け追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :scene_experiences
+  has_many :quiz_attempts
   has_many :active_relationships,  class_name: "Relationship",
                                   foreign_key: "follower_id",
                                     dependent: :destroy
