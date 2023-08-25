2a2df002e31f4b3e33a25f4ccb33de065b32575f
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-08-25 20:34:23 +0900

    チャート％表示追加、ドーナツ型に変更

--- front/components/CustomPieChart.vue
+++ front/components/CustomPieChart.vue
@@ -1,16 +1,22 @@
 <script>
-import { Pie } from 'vue-chartjs'
+import { Doughnut } from 'vue-chartjs'
 
 export default {
-  extends: Pie,
+  extends: Doughnut,
   props: {
     chartData: {
       type: Object,
       default: null
     }
   },
+  methods: {
+    getPercentage: function (value, total) {
+      return ((value / total) * 100).toFixed(0) + '%'
+    }
+  },
   mounted () {
-    // Overwriting base render method with actual data.
+    const totalValue = Object.values(this.chartData).reduce((a, b) => a + b, 0)
+
     this.renderChart({
       labels: Object.keys(this.chartData),
       datasets: [
@@ -20,7 +26,20 @@ export default {
           hoverBackgroundColor: ['#f87979', '#7C4DFF', '#FFC107', '#8BC34A', '#FF5722'] // You can customize the colors here
         }
       ]
-    }, { responsive: true, maintainAspectRatio: false })
+    }, {
+      responsive: true,
+      maintainAspectRatio: false,
+      tooltips: {
+        callbacks: {
+          label: (tooltipItem, data) => {
+            const label = data.labels[tooltipItem.index]
+            const value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
+            const percentage = this.getPercentage(value, totalValue)
+            return `${label}: ${percentage}`
+          }
+        }
+      }
+    })
   }
 }
 </script>
