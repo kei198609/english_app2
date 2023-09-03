a7660932285b202a3e53a3da34c009d06d8cb5fb
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-09-03 13:27:26 +0900

    ArticleReadingsController作成

---
+++ app/controllers/api/v1/article_readings_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::ArticleReadingsController < ApplicationController
+  def create
+    @article_reading = current_user.article_readings.new(article_reading_params)
+    if @article_reading.save
+      current_user.points += 10
+      current_user.save!
+
+      update_level(current_user)
+      render json: { message: 'Article read recorded successfully' }, status: :created
+    else
+      render json: { errors: @article_reading.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_reading_params
+    params.require(:article_reading).permit(:article_id, :read)
+  end
+
+  def update_level(user)
+    level_thresholds = [100,200,300,400,500,600,700,800,900,1000]
+    level_thresholds.each_with_index do |threshold, index|
+      if user.points >= threshold && user.level == index
+        user.update!(level: index + 1)
+      end
+    end
+  end
