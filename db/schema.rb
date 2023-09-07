1f7cb1baf0fbdd3f41017bccda748656888ab055
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-09-07 20:50:18 +0900

    category_name_articleフィールド追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_235552) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_035009) do
   create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.text "body", size: :long
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_235552) do
     t.string "category_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category_name_article"
   end
 
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
