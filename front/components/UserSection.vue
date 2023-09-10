5c7abeead5cafd7e9f0810ae7d0d2c3e2bad7183
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-09-11 06:41:42 +0900

    LevelGaugePointコンポーネント追加

--- front/components/UserSection.vue
+++ front/components/UserSection.vue
@@ -1,42 +1,46 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col class="mt-3">
-        <v-card class="pa-4">
-          <div class="image-container">
-            <v-avatar size="150" class="rounded-circle">
-              <img
-                :src="user.avatar && user.avatar.url ? user.avatar.url : noimage"
-                alt="User Image"
-                class="circle-size"
-              />
-            </v-avatar>
-          </div>
-          <div class="text-center mb-1">{{ user.name }}</div>
-          <div class="text-center border-bottom mb-3">{{ user.occupation }}</div>
-          <div class="text-center">
-            <StatsComponent :user="user" />
-          </div>
-          <div class="text-center mt-3" v-if="userSignedIn">
-            <FollowForm :user="user" />
-          </div>
-          <div class="text-center">
-            <v-btn v-if="isCurrentUser" :to="`/top/${user.id}/profile`" color="primary">プロフィールを編集する</v-btn>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-row>
+    <v-col>
+      <v-card class="pa-4">
+        <div class="text-center mb-3">あなたの情報</div>
+        <div class="image-container">
+          <v-avatar size="150" class="rounded-circle">
+            <img
+              :src="user.avatar && user.avatar.url ? user.avatar.url : noimage"
+              alt="User Image"
+              class="circle-size"
+            />
+          </v-avatar>
+        </div>
+        <div class="text-center mb-1">{{ user.name }}</div>
+        <div class="text-center border-bottom mb-1">{{ user.occupation }}</div>
+        <div class="text-center">
+          <LevelGaugePoint v-if="user" :user="user" />
+        </div>
+        <div class="text-center">
+          <StatsComponent :user="user" />
+        </div>
+        <div class="text-center mt-3" v-if="userSignedIn">
+          <FollowForm :user="user" />
+        </div>
+        <div class="text-center">
+          <v-btn v-if="isCurrentUser" :to="`/top/${user.id}/profile`" color="primary">プロフィールを編集する</v-btn>
+        </div>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 import StatsComponent from '~/components/StatsComponent.vue'
 import FollowForm from '~/components/FollowForm.vue'
+import LevelGaugePoint from '~/components/LevelGaugePoint.vue'
 
 export default {
   components: {
     StatsComponent,
-    FollowForm
+    FollowForm,
+    LevelGaugePoint
   },
   data () {
     return {
