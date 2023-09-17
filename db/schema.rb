60167e22a6628f82babf349669011fa76833d358
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-09-17 20:02:27 +0900

    ポストテーブルのフィールド変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_045422) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_16_224129) do
   create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.text "body", size: :long
@@ -116,12 +116,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_045422) do
   end
 
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "scene"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "content_english"
-    t.text "subject_english"
+    t.string "title"
+    t.text "content"
+    t.string "genre"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
