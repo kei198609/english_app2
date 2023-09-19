c4b2c411769f8d6a44041b24ea280ee967d2e608
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-09-19 21:19:31 +0900

    UserSection追加に伴う変更

--- front/pages/top/_id/quiz.vue
+++ front/pages/top/_id/quiz.vue
@@ -13,47 +13,79 @@
     />
     <v-main>
       <v-container>
-        <div>
-          <v-list v-for="category in categories" :key="category.id">
-            <v-list-item>
-              <v-list-item-content>
-                {{ category.category_name }}
-              </v-list-item-content>
-            </v-list-item>
-            <v-list v-for="q in category.quizzes" :key="q.id">
+        <v-row>
+          <v-col cols="12" md="4" lg="4">
+            <UserSection v-if="user" :user="user" />
+          </v-col>
+          <v-col cols="12" md="8" lg="8">
+            <v-card v-for="category in categories" :key="category.id">
               <v-list-item>
-                <!-- クイズタイトルとリンク -->
-                <nuxt-link :to="`/quiz/${q.id}`">{{ q.title }}</nuxt-link>
-                <!-- 正誤表示 -->
-                <span v-if="getQuizAttempt(q.id)">
-                  {{ getQuizAttempt(q.id).correct ? '正解済み' : '不正解' }}
-                </span>
+                <v-list-item-content>
+                  {{ category.category_name }}
+                </v-list-item-content>
               </v-list-item>
-            </v-list>
-          </v-list>
-        </div>
+              <v-list v-for="q in category.quizzes" :key="q.id">
+                <v-list-item>
+                  <!-- クイズタイトルとリンク -->
+                  <nuxt-link :to="`/quiz/${q.id}`">{{ q.title }}</nuxt-link>
+                  <!-- 正誤表示 -->
+                  <span v-if="getQuizAttempt(q.id)">
+                    {{ getQuizAttempt(q.id).correct ? '正解済み' : '不正解' }}
+                  </span>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import UserSection from '~/components/UserSection.vue'
+
 export default {
+  components: {
+    UserSection
+  },
   data () {
     return {
       drawer: null,
-      categories: []
+      user: null,
+      categories: [],
+      quizAttempts: []
     }
   },
   methods: {
     getQuizAttempt (quizId) {
-      return this.quizAttempts.find(attempt => attempt.quiz_id === quizId)
+      // findメソッドの代わりにfilterを使用して、特定のクイズIDのすべての試行結果を取得
+      const attempts = this.quizAttempts.filter(attempt => attempt.quiz_id === quizId)
+      // 最新の試行結果を返す
+      return attempts[attempts.length - 1]
     }
   },
-  async asyncData ({ params, $axios }) {
-    const { data: categories } = await $axios.get('/api/v1/categories')
-    const { data: quizAttempts } = await $axios.get('/api/v1/quiz_attempts')
-    return { categories, quizAttempts }
+  async fetch () {
+    try {
+      if (this.$auth.loggedIn) {
+        // ログインしているユーザーのIDを取得
+        const userId = this.$auth.user.id
+        // ユーザーの詳細情報を取得
+        const userResponse = await this.$axios.get(`/api/v1/users/${userId}`)
+        this.user = userResponse.data.user
+
+        const categoriesResponse = await this.$axios.get('/api/v1/categories')
+        this.categories = categoriesResponse.data
+
+        const quizResponse = await this.$axios.get('/api/v1/quiz_attempts')
+        this.quizAttempts = quizResponse.data
+
+        // ストアからクイズの試行結果を取得
+        await this.$store.dispatch('fetchQuizAttempts')
+      }
+    } catch (error) {
+      console.error('Error fetching data', error)
+    }
   }
 }
 </script>
