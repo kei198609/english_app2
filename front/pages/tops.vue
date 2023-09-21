bc7c524635e03c3f1d8c3466dbf5676caf9f5c82
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-09-21 14:55:48 +0900

    投稿表示デザインを修正

--- front/pages/tops.vue
+++ front/pages/tops.vue
@@ -14,16 +14,6 @@
       <top-navigation-drawer
         :drawer.sync="drawer"
       />
-      <!-- メインコンテンツ -->
-      <v-parallax>
-        <v-img
-          :src="homeImg"
-          alt="homeImg"
-          :aspect-ratio="16/9"
-          gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
-        >
-        </v-img>
-      </v-parallax>
       <v-main class="main-content">
         <v-container>
           <!-- 検索フォーム -->
@@ -41,7 +31,7 @@
           <v-row justify="center">
             <v-col cols="12">
               <v-card-title>
-                ユーザの投稿
+                新着一覧
               </v-card-title>
 
               <div
@@ -51,48 +41,23 @@
                 <v-hover>
                   <template v-slot:default="{ hover }">
                     <v-card
-                      class="mx-auto mb-5 my-card"
-                      color="#22B2C4"
-                      dark
+                      class="mx-auto mb-5"
                       :elevation="hover ? 4 : 2"
                       @click="$router.push(`/post/${post.id}/postdetail`)"
                     >
-                      <v-icon size="x-large"></v-icon>
-                      <v-card-text class="text-h5 font-weight-bold py-2">
-                        {{ post.subject_english }}
-                      </v-card-text>
-                      <v-card-text class="text-h5 font-weight-bold py-2">
-                        {{ post.content_english }}
-                      </v-card-text>
-
-                      <v-card-actions>
-                        <v-list-item class="">
-                          <v-avatar size="50" class="clickable-image rounded-circle">
-                            <img
-                              :src="post.user && post.user.avatar && post.user.avatar.url ? post.user.avatar.url : noimage"
-                              @click="$router.push(`/top/${post.user_id}/mypage`)"
-                              alt="User Image"
-                              class="circle-size clickable-image"
-                            />
-                          </v-avatar>
-                          <v-list-item-content>
-                            <v-list-item-title>
-                              <!-- <nuxt-link class="routerLink" :to="`/top/${post.user_id}/mypage`">{{ post.user.name }}</nuxt-link> -->
-                              {{ post.user.name }}
-                            </v-list-item-title>
-                            <v-list-item-subtitle>投稿日時: {{ formatDate(post.created_at) }}</v-list-item-subtitle>
-                          </v-list-item-content>
-
-                          <!-- <v-list-item-action>
-                            <v-icon class="mr-1">mdi-heart</v-icon>
-                            <span class="subheading mr-2">256</span>
-                            <span class="mr-1">·</span>
-                            <v-icon class="mr-1">mdi-share-variant</v-icon>
-                            <span class="subheading">45</span>
-                          </v-list-item-action> -->
-
-                        </v-list-item>
-                      </v-card-actions>
+                      <v-row class="px-3">
+                        <v-col cols="12" md="3" lg="3">
+                          <v-img :src="getGenreImageUrl(post)" alt="ジャンルの画像" class="genre-image"></v-img>
+                        </v-col>
+                        <v-col class="d-flex flex-column justify-space-between">
+                          <v-card-text class="text-h5 mb-auto">
+                            {{ post.title }}
+                          </v-card-text>
+                          <v-card-actions>
+                            <UserAvatarPost :post="post" :noimage="noimage" />
+                          </v-card-actions>
+                        </v-col>
+                      </v-row>
                     </v-card>
                   </template>
                 </v-hover>
@@ -106,31 +71,41 @@
 </template>
 
 <script>
-import moment from 'moment'
-import homeImg from '~/assets/images/logged-in/home.png'
+import UserAvatarPost from '~/components/UserAvatarPost.vue'
+
 export default {
+  components: {
+    UserAvatarPost
+  },
   data () {
     return {
       drawer: null,
-      homeImg,
       posts: null,
       noimage: require('~/assets/images/noimage.jpg'),
-      searchQuery: null
+      searchQuery: null,
+      genreImageMapping: {
+        ビジネス文書: 'business_documents',
+        英語メール表現: 'english_email_expressions',
+        一般的な英文メッセージ: 'general_english_message'
+      }
     }
   },
   methods: {
-    formatDate (dateString) {
-      return moment(dateString).format('YYYY-MM-DD HH:mm')
-    },
     async searchPosts () {
       console.log('searchPosts is called with query:', this.searchQuery)
       try {
-        // const response = await this.$axios.get('/api/v1/posts', { params: { q: { subject_english_or_content_english_cont: this.searchQuery } } })
         const response = await this.$axios.get('/api/v1/posts', { params: { query: this.searchQuery } })
         this.posts = response.data.posts
       } catch (error) {
         console.error('Error fetching posts:', error)
       }
+    },
+    getGenreImageUrl (post) {
+      if (post && post.genre && this.genreImageMapping[post.genre]) {
+        const imageName = this.genreImageMapping[post.genre]
+        return require(`@/assets/images/${imageName}.jpg`)
+      }
+      return this.noimage // ジャンル画像がない場合にデフォルト画像を表示
     }
   },
   async created () {
@@ -146,25 +121,8 @@ export default {
 </script>
 
 <style lang="scss">
-#tops {
-  .v-parallax__content{
-    padding: 0;
-  }
-  .my-card {
-    max-width: 600px;
-    height: 200px;
-  }
-  .clickable-image {
-    cursor: pointer;
-  }
-}
-.clickable-image {
-  cursor: pointer;
-
-  transition: box-shadow .1s ease-in-out;  // アニメーションを滑らかに
-
-  &:hover {
-    box-shadow: 0 0 10px rgba(255, 255, 255, 1.0);  // 光る効果を追加
-  }
-}
+// .genre-image {
+//   width: 150px;
+//   height: 150px;
+// }
 </style>
