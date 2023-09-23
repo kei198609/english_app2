210a7f50828cff53ac5ed6b9b3b6ff0151a751d0
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-09-24 07:30:57 +0900

    tops.vueの投稿フィードをUserAvatarPostコンポーネント化

---
+++ front/components/UserAvatarPost.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-list-item>
+    <v-avatar size="50" class="clickable-image rounded-circle">
+      <img
+        :src="post.user && post.user.avatar && post.user.avatar.url ? post.user.avatar.url : noimage"
+        @click="$router.push(`/top/${post.user_id}/mypage`)"
+        alt="User Image"
+        class="circle-size clickable-image"
+      />
+    </v-avatar>
+    <v-list-item-content>
+      <v-list-item-title>
+        {{ post.user.name }}
+      </v-list-item-title>
+      <v-list-item-subtitle>投稿日時: {{ formatDate(post.created_at) }}</v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    },
+    noimage: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    formatDate (dateString) {
+      return moment(dateString).format('YYYY-MM-DD HH:mm')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.clickable-image {
+  cursor: pointer;
+  transition: box-shadow .1s ease-in-out;  // アニメーションを滑らかに
+
+  &:hover {
+    box-shadow: 0 0 10px rgb(110, 109, 109);  // 光る効果を追加
+  }
+}
+</style>
