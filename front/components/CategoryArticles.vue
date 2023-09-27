40f21b6732ae87aebd1c2ae80546552e37a9a67d
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-09-28 06:40:03 +0900

    UIブラッシュアップ

--- front/components/CategoryArticles.vue
+++ front/components/CategoryArticles.vue
@@ -5,18 +5,24 @@
         {{ category.category_name_article }}
       </v-list-item-content>
     </v-card>
-    <v-card v-for="article in category.articles" :key="article.id" class="mt-1 px-3 py-5">
+    <v-card v-for="article in category.articles" :key="article.id" class="mt-1">
       <v-list-item :to="`/article/${article.id}`">
         <v-icon class="pr-3">
           {{ article.icon_name }}
         </v-icon>
         <v-list-item-content>
-          <v-list-item-title>
-            {{ article.title }}
-          </v-list-item-title>
-          <v-list-item-subtitle v-if="isRead(article.id)">
-            チェック済み
-          </v-list-item-subtitle>
+          <v-row>
+            <v-col class="flex-grow-1">
+              <v-list-item-title>
+                {{ article.title }}
+              </v-list-item-title>
+            </v-col>
+            <v-col v-if="isRead(article.id)">
+              <v-list-item-subtitle>
+                チェック済み
+              </v-list-item-subtitle>
+            </v-col>
+          </v-row>
         </v-list-item-content>
       </v-list-item>
     </v-card>
