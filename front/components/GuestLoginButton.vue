79cb3c2e3f0026d2b08869fd33070ef350a7389c
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-09-29 21:29:15 +0900

    ゲストログイン実装

---
+++ front/components/GuestLoginButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-btn
+    text
+    class="ml-2 font-weight-bold"
+    color="black"
+    @click="guestLogin"
+  >
+    ゲストログイン
+  </v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    async guestLogin () {
+      try {
+        const response = await this.$axios.post('/api/v1/auth/sessions/guest_sign_in')
+        const { headers, data } = response
+        const token = headers['access-token']
+        const client = headers.client
+        const uid = headers.uid
+        // Set the token for future requests
+        this.$auth.setToken('local', token)
+        this.$axios.setToken(token, 'Bearer')
+
+        this.$auth.setUser({
+          ...data,
+          uid,
+          client
+        })
+        this.$router.push('/tops')
+      } catch (error) {
+        console.error('ゲストログインに失敗しました', error)
+      }
+    }
+  }
+}
+</script>
