6c4cd531a8f8a0c92e0518fcfd22352a270aae7a
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-10-10 06:40:41 +0900

    通知コントローラー修正

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::NotificationsController < ApplicationController
   before_action :authenticate_user!
   def index
     @notifications = current_user.passive_notifications.page(params[:page]).per(10)
-    @notifications.where(checked: false).each do |notification|
-      notification.update(checked: true)
-    end
+    # @notifications.where(checked: false).each do |notification|
+    #   notification.update(checked: true)
+    # end
     render json: @notifications.as_json(
       include: [
         :visitor,
