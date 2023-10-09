9a3e9efb377f91a050cc0076b0b8b5bb2f1e95c3
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-10-09 20:38:50 +0900

    コンポーネント化

---
+++ front/components/UserAvatarBookmarkPost.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-list-item>
+    <v-avatar size="50" class="clickable-image rounded-circle">
+      <img
+        :src="bookmark.post.user.avatar && bookmark.post.user.avatar.url ? bookmark.post.user.avatar.url : noimage"
+        @click.stop="$router.push(`/top/${bookmark.post.user.id}/mypage`)"
+        alt="User Image"
+        class="circle-size clickable-image"
+      />
+    </v-avatar>
+    <v-list-item-content>
+      <v-list-item-title>
+        {{ bookmark.post.user.name }}
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        投稿日時: {{ formatDate(bookmark.post.created_at) }}
+      </v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  props: {
+    bookmark: {
+      type: Object,
+      required: true
+    },
+    noimage: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    formatDate (dateString) {
+      return moment(dateString).format('YYYY-MM-DD HH:mm')
+    }
+  }
+}
+</script>
