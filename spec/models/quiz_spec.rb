40ad7671cbab0c433fb837a61d222caca744a57a
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-10-18 06:41:59 +0900

    モデルrspec作成

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it "has many quiz_attempts" do
+      category = FactoryBot.create(:category)
+      quiz = FactoryBot.create(:quiz, category: category)
+      user = FactoryBot.create(:user)
+
+      quiz_attempt1 = FactoryBot.create(:quiz_attempt, user: user, quiz: quiz)
+      quiz_attempt2 = FactoryBot.create(:quiz_attempt, user: user, quiz: quiz)
+
+      expect(quiz.quiz_attempts).to include(quiz_attempt1, quiz_attempt2)
+    end
+
+    it "belongs to category" do
+      category = FactoryBot.create(:category)
+      quiz = FactoryBot.build(:quiz, category: category)
+      expect(quiz.category).to eq(category)
+    end
+  end
+
+  describe "validations" do
+    # sentence_japaneseの存在性のテスト
+    it "is invalid without a sentence_japanese" do
+      quiz = FactoryBot.build(:quiz, sentence_japanese: nil)
+      quiz.valid?
+      expect(quiz.errors[:sentence_japanese]).not_to be_empty
+    end
+
+    # sentence_englishの存在性のテスト
+    it "is invalid without a sentence_english" do
+      quiz = FactoryBot.build(:quiz, sentence_english: nil)
+      quiz.valid?
+      expect(quiz.errors[:sentence_english]).not_to be_empty
+    end
+  end
 end
