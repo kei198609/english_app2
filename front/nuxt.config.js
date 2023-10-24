89f8499f28b6144731b58d6af68f9e7a2253faf5
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-10-25 06:34:23 +0900

    primaryカラー変更

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -103,7 +103,7 @@ export default {
     theme: {
       themes: {
         light: {
-          primary: '4080BE',
+          primary: '7097C3',
           info: '4FC1E9',
           success: '44D69E',
           warning: 'FEB65E',
