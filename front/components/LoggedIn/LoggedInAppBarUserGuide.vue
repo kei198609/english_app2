4a850cd39f25c5b1af246b7cc599479487032a37
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-11-19 07:19:55 +0900

    使い方ページリンクをバーに追加

---
+++ front/components/LoggedIn/LoggedInAppBarUserGuide.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn
+    icon
+    to="/guide"
+  >
+    <v-icon class="black--text">
+      mdi-help-circle
+    </v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+}
+</script>
