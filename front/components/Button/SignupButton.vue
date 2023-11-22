e659e22796a63c3ddbe51ce83600f83206c11c0b
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-11-23 06:29:26 +0900

    LPページ用の会員登録ボタン、ゲストログインボタン作成

---
+++ front/components/Button/SignupButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    text
+    x-large
+    class="ml-2 font-weight-bold custom-button-signup white--text"
+    color="black"
+    to="/signup"
+  >
+    {{ $my.pageTitle('signup') }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style>
+.custom-button-signup {
+  background-color: black;
+  width: 300px;
+  border-radius: 0;
+}
+</style>
---
+++ front/components/Button/TryLoginButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-btn
+    text
+    x-large
+    class="ml-2 font-weight-bold custom-button-try"
+    @click="guestLogin"
+  >
+    無料でお試し
+  </v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    async guestLogin () {
+      try {
+        // Authモジュールの`loginWith`メソッドを使用してゲストログイン
+        await this.$auth.loginWith('local', {
+          data: {},
+          url: '/api/v1/auth/sessions/guest_sign_in'
+        })
+      } catch (error) {
+        console.error('ゲストログインに失敗しました', error)
+      }
+    }
+  }
+}
+</script>
+<style>
+.custom-button-try {
+  background-color: #ffbf00;
+  width: 300px;
+  border-radius: 0;
+}
+</style>
