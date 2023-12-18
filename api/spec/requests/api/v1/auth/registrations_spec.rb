4a74f00ed119eb6204e246506f523e43ee309672
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-12-19 06:28:33 +0900

    registrations_spec作成

--- api/spec/requests/api/v1/auth/registrations_spec.rb
+++ api/spec/requests/api/v1/auth/registrations_spec.rb
@@ -1,7 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Auth::Registrations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /auth" do
+    let(:valid_user_params) do
+      {
+        name: 'Test User',
+        email: 'user@example.com',
+        password: 'password123',
+        occupation: 'Developer',
+        password_confirmation: 'password123'
+      }
+    end
+    context "有効なパラメータの場合" do
+      it "新規ユーザーを作成する" do
+        expect {
+          post '/api/v1/auth', params: { user: valid_user_params }
+        }.to change(User, :count).by(1)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+    context "無効なパラメータの場合" do
+      it "名前が欠けている場合ユーザーは作成されない" do
+        invalid_params = valid_user_params.merge(name: nil)
+        expect {
+          post '/api/v1/auth', params: { user: invalid_params }
+        }.not_to change(User, :count)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+      it "パスワードとパスワード確認が一致しない場合ユーザーは作成されない" do
+        invalid_params = valid_user_params.merge(password_confirmation: 'wrong')
+        expect {
+          post '/api/v1/auth', params: { user: invalid_params }
+        }.not_to change(User, :count)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+      it "既に取得されているメールアドレスを使用した場合ユーザーは作成されない" do
+        create(:user, email: valid_user_params[:email])
+        expect {
+          post '/api/v1/auth', params: { user: valid_user_params }
+        }.not_to change(User, :count)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 end
