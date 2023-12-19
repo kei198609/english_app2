b6e7d54a6570dc9ccf80bbe37b84e7e64e238d1b
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-12-20 06:30:10 +0900

    sessions_spec作成

--- api/spec/requests/api/v1/auth/sessions_spec.rb
+++ api/spec/requests/api/v1/auth/sessions_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Auth::Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /auth/sessions/guest_sign_in" do
+    it "ゲストユーザーとしてサインインすると成功する" do
+      post '/api/v1/auth/sessions/guest_sign_in'
+
+      expect(response).to have_http_status(:ok)
+      expect(json['data']).to include('id', 'email', 'name')
+    end
+  end
+  def json
+    JSON.parse(response.body)
   end
 end
