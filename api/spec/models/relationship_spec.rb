671cda8ee0626aa83cb0da3b15b29eacfa7a7895
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-12-24 07:15:28 +0900

    テストの説明文を日本語に変更

--- api/spec/models/relationship_spec.rb
+++ api/spec/models/relationship_spec.rb
@@ -6,17 +6,14 @@ RSpec.describe Relationship, type: :model do
   let(:relationship) { Relationship.new(follower: follower, followed: followed) }
 
   describe "validations" do
-    # Relationshipモデルのインスタンスが有効であること
-    it "is valid with valid attributes" do
+    it "正しい属性を持っている場合、有効である" do
       expect(relationship).to be_valid
     end
-    # follower_idがnilである場合、Relationshipモデルのインスタンスが無効であること
-    it "is not valid without a follower_id" do
+    it "フォロワーIDが存在しない場合、無効である" do
       relationship.follower_id = nil
       expect(relationship).to_not be_valid
     end
-    # followed_idがnilである場合、Relationshipモデルのインスタンスが無効であること
-    it "is not valid without a followed_id" do
+    it "フォローされるユーザーIDが存在しない場合、無効である" do
       relationship.followed_id = nil
       expect(relationship).to_not be_valid
     end
@@ -24,12 +21,10 @@ RSpec.describe Relationship, type: :model do
 
   # Relationshipモデルがfollowerとfollowedという2つの関連付けを持っていることを確認
   describe "associations" do
-    # Relationshipモデルのfollowerが正しく関連付けられていること
-    it "belongs to a follower" do
+    it "フォロワーに正しく関連付けられている" do
       expect(relationship.follower).to eq(follower)
     end
-    # Relationshipモデルのfollowedが正しく関連付けられていること
-    it "belongs to a followed user" do
+    it "フォローされるユーザーに正しく関連付けられている" do
       expect(relationship.followed).to eq(followed)
     end
   end
