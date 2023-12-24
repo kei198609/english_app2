a2466c3db6e1a0991d2f9ddda8a1a847fe623bd5
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-12-25 06:29:25 +0900

    テストの説明文を日本語に変更

--- api/spec/models/comment_spec.rb
+++ api/spec/models/comment_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  # コメントがなければ無効な状態であること
-  it "is invalid without comment" do
+  it "コメントがない場合は無効である" do
     comment = FactoryBot.build(:comment, comment: nil)
     comment.valid?
     expect(comment.errors[:comment]).to include("が入力されていません。")
   end
-  #コメントが100文字以上なら無効な状態であること
-  it "is comment should not be too long" do
+  it "コメントが100文字を超える場合は無効である" do
     comment = FactoryBot.build(:comment, comment: "a" * 101)
     comment.valid?
     expect(comment.errors[:comment]).to include("の入力文字数は100文字以内です。")
