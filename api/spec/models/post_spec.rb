b327c7336094258d70ee7d1f317096970770c6d5
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-12-26 06:28:54 +0900

    テストの説明文を日本語に変更

--- api/spec/models/post_spec.rb
+++ api/spec/models/post_spec.rb
@@ -1,32 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  # ジャンルがなければ無効な状態であること
-  it "is invalid without genre" do
+  it "genreがない場合は無効である" do
     post = FactoryBot.build(:post, genre: nil)
     post.valid?
     expect(post.errors[:genre]).not_to be_empty
   end
-  # タイトルがなければ無効な状態であること
-  it "is invalid without title" do
+  it "titleがない場合は無効である" do
     post = FactoryBot.build(:post, title: nil)
     post.valid?
     expect(post.errors[:title]).not_to be_empty
   end
-  # 内容がなければ無効な状態であること
-  it "is invalid without content" do
+  it "contentがない場合は無効である" do
     post = FactoryBot.build(:post, content: nil)
     post.valid?
     expect(post.errors[:content]).not_to be_empty
   end
-  # タイトルが100文字以上なら無効な状態であること
-  it "is title should not be too long" do
+  it "titleが100文字を超える場合は無効である" do
     post = FactoryBot.build(:post, title: "a" * 101)
     post.valid?
     expect(post.errors[:title]).not_to be_empty
   end
-  # 内容が2000文字以上なら無効な状態であること
-  it "is content should not be too long" do
+  it "contentが2000文字を超える場合は無効である" do
     post = FactoryBot.build(:post, content: "a" * 2001)
     post.valid?
     expect(post.errors[:content]).not_to be_empty
