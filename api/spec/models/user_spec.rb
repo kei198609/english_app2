0ccdda31c87c601760f61ca2e8b40014a980b405
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-12-30 07:09:24 +0900

    テストの説明文を日本語に変更

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -1,53 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # 名前、メール、職種、パスワードがあれば有効な状態であること
-  it "is valid with a name, email, and password" do
+  it "名前、メール、職種、パスワードがある場合は有効" do
     expect(FactoryBot.build(:user)).to be_valid
   end
-  # メールアドレスがなければ無効な状態であること
-  it "is invalid without an email address" do
+  it "メールアドレスがない場合は無効" do
     user = FactoryBot.build(:user, email: nil)
     expect(user).to_not be_valid
   end
-
-  # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address" do
+  it "重複したメールアドレスの場合は無効" do
     FactoryBot.create(:user, email: "aaron@example.com")
     user = FactoryBot.build(:user, email: "aaron@example.com")
     user.valid?
     expect(user.errors[:email]).to include("は既に登録済みです。")
   end
 
-  # 名前がなければ無効な状態であること
-  it "is invalid without an name" do
+  it "名前がない場合は無効" do
     user = FactoryBot.build(:user, name: nil)
     user.valid?
     expect(user.errors[:name]).to include("が入力されていません。")
   end
-  # 職種がなければ無効な状態であること
-  it "is invalid without an occupation" do
+  it "職種がない場合は無効" do
     user = FactoryBot.build(:user, occupation: nil)
     user.valid?
     expect(user.errors[:occupation]).to include("が入力されていません。")
   end
-  # メールアドレスがなければ無効な状態であること
-  it "is invalid without an email" do
+  it "メールアドレスがない場合は無効" do
     user = FactoryBot.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("が入力されていません。")
   end
-  #名前が10文字以上なら無効な状態であること
-  it "is name should not be too long" do
+  it "名前が10文字を超える場合は無効" do
     user = FactoryBot.build(:user, name: "a" * 11)
     user.valid?
     expect(user.errors[:name]).to include("は10文字以内で入力してください。")
   end
-  #パスワードが6文字未満なら無効な状態であること
-  it "is password should not be too short" do
+  it "パスワードが6文字未満の場合は無効" do
     user = FactoryBot.build(:user, password: "a" * 5)
     user.valid?
     expect(user.errors[:password]).to include("は6文字以上で入力してください。")
   end
-
 end
