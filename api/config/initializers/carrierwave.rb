cc9c1158a4d4eec27acd75ef6c5bcff5ff130a40
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-12-31 07:18:58 +0900

    carrierwave.rbをS3対応のため修正

--- api/config/initializers/carrierwave.rb
+++ api/config/initializers/carrierwave.rb
@@ -1,3 +1,24 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
-  config.asset_host = 'http://localhost:3000'
+
+  if Rails.env.production? # 本番環境の場合はS3へアップロード
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'バケット名' # 作成したバケット名を記述
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
+      region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
+      path_style: true
+    }
+  else
+    # 開発環境はlocalに保存
+    config.storage :file
+    config.asset_host = 'http://localhost:3000'
+  end
+
 end
