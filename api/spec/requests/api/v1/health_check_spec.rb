b10dc4ae835600cf3a32544877201f5097e263d7
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-01-11 07:53:59 +0900

    create health_check_controller

--- api/spec/requests/api/v1/health_check_spec.rb
+++ api/spec/requests/api/v1/health_check_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Api::V1::HealthChecks", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/api/v1/health_check/index"
+      get "/api/v1/health_check"
       expect(response).to have_http_status(:success)
     end
   end
