406b20a7f48211cd9c6b257ab7c5da056a3809b7
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-01-26 20:13:16 +0900

    change entrypoint.sh

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
-# WARNING:createとseedはfargateの初回のみ実行
+# WARNING:createとseedはfargateの初回のみ実行,開発環境の時はコメントアウト
 # bundle exec rails db:create RAILS_ENV=production
-# bundle exec rails db:migrate RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production
 # bundle exec rails db:seed RAILS_ENV=production
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
