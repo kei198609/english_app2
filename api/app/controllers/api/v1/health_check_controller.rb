151b196d5f1188fc59c1b72db81d758fb223cf0c
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-02-17 07:06:12 +0900

    change for health check controller

--- api/app/controllers/api/v1/health_check_controller.rb
+++ api/app/controllers/api/v1/health_check_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::HealthCheckController < ApplicationController
   def index
-    logger.info "============================================================"
-    logger.info "ReuqestUrl: #{request.url}"
-    render json: {status: 200}, status: 200
+    render json: { status: 200 }, status: 200
   end
 end
