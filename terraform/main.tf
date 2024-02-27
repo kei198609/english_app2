b72728c80d5fd6818816d998152e86fccda680e2
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-02-27 15:00:41 +0900

    create for main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 4.1.0"
+    }
+  }
+}
+
+locals {
+  app_name = "mailmentor"
+  host_domain = "mailmentor.link"
+  app_domain_name = "mailmentor.link"
+  api_domain_name = "api-mailmentor.link"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      application = local.app_name
+    }
+  }
+}
