0f0964f2d033fb41c7478c4ec46f1a3a3a71e328
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-02-29 06:27:08 +0900

    change for main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 locals {
-  app_name = "mailmentor"
+  # app_name = "mailmentor"
   host_domain = "mailmentor.link"
   app_domain_name = "mailmentor.link"
   api_domain_name = "api-mailmentor.link"
@@ -16,9 +16,9 @@ locals {
 
 provider "aws" {
   region = "ap-northeast-1"
-  default_tags {
-    tags = {
-      application = local.app_name
-    }
-  }
+  # default_tags {
+  #   tags = {
+  #     application = local.app_name
+  #   }
+  # }
 }
