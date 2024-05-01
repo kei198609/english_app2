0f8ce8d1b21cadc99e98573be30e1cded16cd1b7
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-05-01 13:43:16 +0900

    UserFormCard.vueの文字を一部削除

--- front/components/User/UserFormCard.vue
+++ front/components/User/UserFormCard.vue
@@ -9,7 +9,7 @@
         class="my-8 text-center"
       >
         <h1 class="text-h5 font-weight-bold">
-          {{ appName }}に{{ pageTitle }}
+          {{ appName }}{{ pageTitle }}
         </h1>
       </v-col>
 
